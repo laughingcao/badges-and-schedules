44e55d998a5a82352e94273551c041bb216ce2b8
William Cao <williamacao@gmail.com>
2021-01-16 14:05:37 -0800

    commit tree ec7e6bdcb81683881ea3c69c95c3232286b7e2aa

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map {|x| badge_maker(x)}
+end
+
+def assign_rooms(attendees)
+   new_arr = []
+   attendees.each_with_index do | name, i|
+    new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+   end
+   new_arr
+end
+
+def printer(names)
+    batch_badge_creator(names).each {|x| puts x}
+    assign_rooms(names).each {|x| puts x}
+end
